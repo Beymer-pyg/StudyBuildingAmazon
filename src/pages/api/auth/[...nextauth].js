d70073057a71e5b1776587d20cea3c2d9eba404d
Beymer Bustamante <bustamantef@unprg.edu.pe>
2023-09-15 23:22:59 -0500

    next auth solo google

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-//import Providers from "next-auth/providers";
-import FacebookProvider from "next-auth/providers/facebook";
+/* import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook"; */
 
+//import Providers from "next-auth/providers";
 export const authOptions = {
   //configure one or more authentication providers
   providers: [
@@ -11,14 +11,14 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    GithubProvider({
+    /* GithubProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     FacebookProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
+    }), */
     // ...add more providers here
   ],
 };
